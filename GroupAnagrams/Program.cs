67b216620a515dc549937539294c3c706620a0ac
Serhii Hrytsenko <serhii.hrytsenko@dataart.com>
2021-11-06 00:27:03 +0200

    added solition for group anagrams

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GroupAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+            var solution = new Solution();
+            solution.GroupAnagrams(strs);
+        }
+    }
+}
---
+++ GroupAnagrams/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupAnagrams
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new List<IList<string>>();
+            var groupDict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var orderedCurrent = string.Concat(strs[i].OrderBy(c => c));
+
+                if (!groupDict.ContainsKey(orderedCurrent))
+                    groupDict.Add(orderedCurrent, new List<string> { strs[i] });
+
+                else
+                    groupDict[orderedCurrent].Add(strs[i]);
+            }
+            result.AddRange(groupDict.Values);
+            return result;
+        }
+    }
+}
